265ba205feeb6628acd274644371e13479aabd42
wwwoojung <50447560+wwwoojung@users.noreply.github.com>
2023-11-13 23:24:37 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -5,8 +5,14 @@ $(function () {
         pauseOnHover: false,
     });
 
-    $('.main_visual .visual_slide').on('init afterchange', function (e, s, c) {
-        const current = $('.visual_slide .slick-current')
-        console.log
+    $('.main_visual .visual_slide').on('init afterChange e', function (e, s, c) {
+        const current = $('.visual_slide .slick-current');
+        console.log(c, s.slideCount);
+
+        $('.main_visual .slide_box .slide_num .slides').text(s.slideCount);
+        $('.main_visual .slide_box .slide_num .current').text(c ? (c + 1) : 1);
+
+        $('.main_visual .slide_box .slide_text li').eq(c).addClass('on')
+            .siblings().removeClass('on');
     })
 })
