134454e4522e05eaccad06c707b64646f1e31b43
wwwoojung <50447560+wwwoojung@users.noreply.github.com>
2023-11-17 17:28:50 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -9,29 +9,65 @@ $(function () {
         };
     })
 
+    $('.main_visual .visual_slide').on('init', function () {
+        const current = $('.visual_slide .slick-current');
+
+        const a = new Array(6);
+        for (let i = 0; i < a.length; i++) {
+            current.find('.slide').append('<div class="s"><span class="bg"></span></div>')
+        }
+
+        const span = $('.slide .bg');
+        span.each(function (idx, itm) {
+            $(itm).css({
+                background: `url(../images/main_slide01.jpg) no-repeat calc((-100vw / ${a.length} * ${idx})) center`
+            }).animate({ width: '100%' })
+        })
+
+        current.addClass('on').siblings().removeClass('on');
+    })
+
     $('.main_visual .visual_slide').slick({
         arrows: false,
         autoplay: true,
         pauseOnHover: false,
         autoplaySpeed: 4000,
+        speed: 0,
+        fade: true,
     });
 
-    $('.main_visual .visual_slide').on('init afterChange e', function (e, s, c) {
+    $('.main_visual .visual_slide').on('beforeChange', function () {
+        $('.circle').removeClass('on')
+        $('.slick-slide').find('.slide .bg').css({ width: 0 });
+        $('.slick-slide').find('.slide').children().remove();
+    })
+
+    $('.main_visual .visual_slide').on('afterChange e', function (e, s, c) {
         const current = $('.visual_slide .slick-current');
         console.log(c, s.slideCount);
 
         $('.main_visual .slide_box .slide_num .slides').text(s.slideCount);
         $('.main_visual .slide_box .slide_num .current').text(c ? (c + 1) : 1);
 
-        $('.main_visual .slide_box .slide_text li').eq(c).addClass('on')
-            .siblings().removeClass('on');
+        $('.main_visual .slide_box .slide_text li').removeClass('on');
+        $('.main_visual .slide_box .slide_text li').eq(c).addClass('on');
 
         $('.circle').addClass('on');
-    });
 
-    $('.main_visual .visual_slide').on('beforeChange', function () {
-        $('.circle').removeClass('on')
-    })
+        const a = new Array(6);
+        for (let i = 0; i < a.length; i++) {
+            current.find('.slide').append('<div class="s"><span class="bg"></span></div>')
+        }
+
+        const span = $('.slide .bg');
+        span.each(function (idx, itm) {
+            $(itm).css({
+                background: `url(../images/main_slide0${c + 1}.jpg) no-repeat calc((-100vw / ${a.length} * ${idx})) center`
+            }).animate({ width: '100%' })
+        })
+
+        current.addClass('on').siblings().removeClass('on');
+    });
 
     $('.main_visual .slide_box .control .left').on('click', function () {
         $('.main_visual .visual_slide').slick('slickPrev')
@@ -59,44 +95,25 @@ $(function () {
         e.preventDefault()
     })
 
-    $('#promotion').YTPlayer({
+    $('#video').YTPlayer({
         videoURL: 'https://youtu.be/-PiJlB3JJ9Y',
-        containment: '.main_TV .movie_case',
-        showControls: false,
-        optimizeDisplay: false,
-        playOnlyIfVisible: true,
+        containment: '.video_box',
         autoPlay: false,
+        optimizeDisplay: false,
+        showControls: false,
+        playOnlyIfVisible: true,
     });
 
+    let play = true
+    $('.main_cm .video_box .control').on('click', function () {
+        $(this).toggleClass('on')
 
-
-    let movie = true;
-
-
-    $('.movie_play').on('click', function () {
-        if (movie) {
-            $('#promotion').YTPPlay();
-        }
-        else {
-            $('#promotion').YTPPause();
-        }
-
-        $(this).toggleClass('on');
-
-        movie = !movie;
-    })
-
-    $(window).on('scroll', function () {
-        const sct = $(this).scrollTop();
-
-        if (sct > 200) {
-            $('.totop').addClass('on')
+        if (play) {
+            $('#video').YTPPlay();
         } else {
-            $('.totop').removeClass('on')
+            $('#video').YTPPause();
         }
-    })
 
-    $('.totop').on('click', function () {
-        $('html, body').animate({ scrollTop: 0 }, 500);
+        play = !play;
     })
 })
